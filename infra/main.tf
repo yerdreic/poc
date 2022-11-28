e4136ae2a0643ccb8d0f85ce83c9fbc4bb6c7548
yerdreic <yerdreic@redhat.com>
2022-11-28 18:12:41 +0200

    Fixed the variables + added destroy option in makefile to destroy the infra
    1. Added variable.tf - which declares the variables will be used with default values
    2. The final value of the vairables, if declared, is determined in the terraform.tfvars file
    - check out this https://spacelift.io/blog/terraform-tfvars for more detailes
    3. Added "destroy" functionality in the makefile

--- infra/main.tf
+++ infra/main.tf
@@ -1,23 +1,3 @@
-locals {
-  PROJECT_NAME          = "poc"
-  SSH_KEY_NAME          = "poc-aws-key"
-  SSH_PRIVATE_FILE_NAME = "poc-aws-key.pem"
-  SG_NAME               = "poc-aws"
-  SG_TAG_KEY            = "Name"
-  SG_TAG_VAL            = "poc-aws"
-  SG_ID                 = ""
-  IMAGE_ID              = "ami-0caef02b518350c8b"
-  EC2_TYPE              = "t2.micro"
-  EC2_TAG_KEY           = "Name"
-  EC2_TAG_VAL           = "poc-aws-server"
-  SG_DESCRIPTION        = "AWS SG for poc-aws"
-  AWS_REGION            = "eu-central-1"
-  IAM_ROLE_NAME         = "PocRole"
-  POLICY_NAME           = "PocPolicy"
-  PROFILE_NAME          = "PocProfile"
-
-}
-
 terraform {
   required_providers {
     aws = {
@@ -30,12 +10,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region = var.aws_region
 }
 
 resource "aws_instance" "poc_instance" {
-  ami           = locals.IMAGE_ID
-  instance_type = locals.EC2_TYPE
+  ami           = var.image_id
+  instance_type = var.ec2_type
 
   tags = {
     Name = "HelloWorldInstance"
---
+++ infra/variable.tf
@@ -0,0 +1,79 @@
+variable "project_name" {
+    type = string
+    default = "poc"
+}
+
+variable "ssh_key_name"{
+    type = string
+    default = "poc-aws-key"
+}
+
+variable "ssh_private_file_name"{
+    type = string
+    default = "poc-aws-key.pem"
+}
+
+variable "sg_name"{
+    type = string
+    default = "poc-aws"
+}
+
+variable "sg_tag_key"{
+    type = string
+    default = "Name"
+}
+
+variable "sg_tag_val"{
+    type = string
+    default = "poc-aws"
+}
+
+variable "sg_id"{
+    type = string
+    default = ""
+}
+
+variable "image_id"{
+    type = string
+    default = "ami-0caef02b518350c8b"
+}
+
+variable "ec2_type"{
+    type = string
+    default = "t2.micro"
+}
+
+variable "ec2_tag_key"{
+    type = string
+    default = "Name"
+}
+
+variable "ec2_tag_val"{
+    type = string
+    default = "poc-aws-server"
+}
+
+variable "sg_description"{
+    type = string
+    default = "AWS SG for poc-aws"
+}
+
+variable "aws_region"{
+    type = string
+    default = "eu-central-1"
+}
+
+variable "iam_role_name"{
+    type = string
+    default = "PocRole"
+}
+
+variable "policy_name"{
+    type = string
+    default = "PocPolicy"
+}
+
+variable "profile_name"{
+    type = string
+    default = "PocProfile"
+}
